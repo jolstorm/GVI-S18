0d986069727f2339ae333e91e4679449b1ed3444
jolstorm <gaurav.virmani@sparkeighteen.com>
2022-05-10 02:18:07 +0530

    Accordion

---
+++ check.js
@@ -0,0 +1,30 @@
+// for (let i = 0; i <= 4; i += 1) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 0);
+// }
+
+// let o = {
+//   x: 5,
+//   y: function (y) {
+//     console.log(this);
+//     return this.x + y;
+//   },
+// };
+
+// const c = {
+//   x: 10,
+// };
+// console.log(o.y.call(c, 10));
+
+// let arr = [2, 3, 4, 5, 6];
+// arr.length = 0;
+// console.log();
+
+function a(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+console.log(a(5)(10));
