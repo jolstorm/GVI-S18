6b00e151c38c01f8d26e5860cd9d9e377e450239
jolstorm <gaurav.virmani@sparkeighteen.com>
2022-05-28 16:39:38 +0530

    Navbar hover feature. Hero images auto change and change on click.

---
+++ index.js
@@ -0,0 +1,49 @@
+let current = 1;
+let prev = 1;
+let interval = 3;
+const products = document.querySelectorAll("#hero .products-cont .product");
+products[current].lastElementChild.classList += " selected";
+const heroImgs = document.getElementsByClassName("hero-images");
+
+let change = setInterval(check, interval * 1000);
+
+function check() {
+  products[current ? current - 1 : 2].lastElementChild.classList.remove(
+    "selected"
+  );
+  products[current].lastElementChild.classList += " selected";
+  heroImgs[current ? current - 1 : 2].style.opacity = "0";
+  heroImgs[current].style.opacity = "1";
+  prev = current;
+  current++;
+  if (current == heroImgs.length) {
+    current = 0;
+  }
+}
+
+products.forEach((product, index) => {
+  product.addEventListener("click", () => {
+    clearInterval(change);
+
+    products[prev].lastElementChild.classList.remove("selected");
+    product.lastElementChild.classList += " selected";
+    heroImgs[prev].style.opacity = "0";
+    heroImgs[index].style.opacity = "1";
+
+    current = index;
+    prev = index;
+    change = setInterval(check, interval * 1000);
+  });
+});
+
+const nav = document.querySelectorAll("nav a");
+for (const a of nav) {
+  if (!a.hasAttribute("id")) {
+    a.addEventListener("mouseover", () => {
+      a.classList += " hov";
+    });
+    a.addEventListener("mouseleave", () => {
+      a.classList.remove("hov");
+    });
+  }
+}
