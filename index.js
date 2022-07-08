2ab794453f88424a79d2941c16b6f9392896a368
jolstorm <gaurav.virmani@sparkeighteen.com>
2022-07-08 17:30:12 +0530

    ft: Make sections fade in. Make likes and bookmarks using event delegation.

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const likes = document.getElementsByClassName("heart-circle");
 const bookmarks = document.querySelectorAll(".bookmark");
 const content = document.querySelectorAll(".content");
 content[1].style.opacity = "1";
-console.log(window.innerWidth);
 let change = setInterval(check, interval * 1000);
 const spcd = document.querySelectorAll(".single-product-card .product-details");
 function check() {
@@ -49,7 +48,6 @@ products.forEach((product, index) => {
 });
 
 const nav = document.querySelectorAll("nav a");
-console.log(nav);
 for (const a of nav) {
   if (!a.hasAttribute("id")) {
     a.addEventListener("mouseover", () => {
@@ -61,17 +59,38 @@ for (const a of nav) {
   }
 }
 
-for (const bookmark of bookmarks) {
-  bookmark.addEventListener("click", () => {
-    bookmark.classList.toggle("bookmarked");
-  });
-}
+//Normal Method
 
-for (const like of likes) {
-  like.addEventListener("click", () => {
-    like.classList.toggle("liked");
-  });
-}
+// for (const bookmark of bookmarks) {
+//   bookmark.addEventListener("click", () => {
+//     bookmark.classList.toggle("bookmarked");
+//   });
+// }
+
+// for (const like of likes) {
+//   like.addEventListener("click", () => {
+//     like.classList.toggle("liked");
+//   });
+// }
+
+//event bubbling and delegation
+document.getElementById("blog").addEventListener("click", (e) => {
+  if (e.target.classList.contains("bookmark")) {
+    e.target.classList.toggle("bookmarked");
+  }
+  if (e.target.classList.contains("fa-bookmark")) {
+    e.target.parentNode.classList.toggle("bookmarked");
+  }
+});
+
+document.querySelector(".products").addEventListener("click", (e) => {
+  if (e.target.classList.contains("heart-circle")) {
+    e.target.classList.toggle("liked");
+  }
+  if (e.target.classList.contains("fa-heart")) {
+    e.target.parentNode.classList.toggle("liked");
+  }
+});
 
 document.getElementById("hamburger").addEventListener("click", () => {
   document.querySelector(".ddmenu-wrapper").classList += " show";
@@ -82,3 +101,40 @@ document.querySelector("#close i").addEventListener("click", () => {
   document.querySelector(".ddmenu-wrapper").classList.remove("show");
   document.querySelector(".shade").classList.remove("dark");
 });
+
+const sections = document.querySelectorAll("section+section");
+
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: calculateThreshold(),
+};
+let count = 0;
+let callback = (entries, observer) => {
+  console.log("yolo");
+  const [entry] = entries;
+  if (count == 0) {
+    count++;
+    return;
+  }
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+let observer = new IntersectionObserver(callback, options);
+sections.forEach((section) => {
+  console.log("hiding section");
+  section.classList.add("section--hidden");
+  observer.observe(section);
+});
+
+function calculateThreshold() {
+  console.log("CYolo");
+  let width = window.innerWidth;
+  if (width <= 425) {
+    return 0.01;
+  } else if (width > 425 && width <= 768) {
+    return 0.02;
+  } else {
+    return 0.05;
+  }
+}
